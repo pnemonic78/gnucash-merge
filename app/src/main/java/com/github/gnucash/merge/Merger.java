f63313d1ffef4f0b8c468f8f5db27b587f503f79
Moshe <moshe.waisberg@tikalk.com>
2017-05-10 14:40:20 +0300

    hello.

---
+++ app/src/main/java/com/github/gnucash/merge/Merger.java
@@ -0,0 +1,16 @@
+/*
+ * GNU General Public License v3.0
+ * Copyright (C) 2017, Moshe Waisberg
+ */
+package com.github.gnucash.merge;
+
+/**
+ * Merge gnucash files.
+ *
+ * @author Moshe Waisberg
+ */
+public class Merger {
+    public static void main(String[] args) {
+        System.out.println("gnucash merge.");
+    }
+}
