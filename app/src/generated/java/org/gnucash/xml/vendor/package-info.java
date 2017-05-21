53579abbc83f92a5783a05ab1e5587612fb2ccab
Moshe <moshe.waisberg@tikalk.com>
2017-05-21 10:12:54 +0300

    namespaces.

--- app/src/generated/java/org/gnucash/xml/vendor/package-info.java
+++ app/src/generated/java/org/gnucash/xml/vendor/package-info.java
@@ -1,4 +1,33 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.gnucash.org/XML/vendor", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {@XmlNs(namespaceURI = "http://www.gnucash.org/XML/vendor", prefix = "vendor")})
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.gnucash.org/XML/vendor", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/act", prefix = "act"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/addr", prefix = "addr"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/bgt", prefix = "bgt"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/billterm", prefix = "billterm"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/book", prefix = "book"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/bt-days", prefix = "bt-days"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/bt-prox", prefix = "bt-prox"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/cd", prefix = "cd"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/cmdty", prefix = "cmdty"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/cust", prefix = "cust"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/employee", prefix = "employee"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/entry", prefix = "entry"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/gnc", prefix = "gnc"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/invoice", prefix = "invoice"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/job", prefix = "job"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/lot", prefix = "lot"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/order", prefix = "order"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/owner", prefix = "owner"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/price", prefix = "price"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/recurrence", prefix = "recurrence"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/slot", prefix = "slot"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/split", prefix = "split"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/sx", prefix = "sx"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/taxtable", prefix = "taxtable"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/trn", prefix = "trn"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/ts", prefix = "ts"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/tte", prefix = "tte"),
+        @XmlNs(namespaceURI = "http://www.gnucash.org/XML/vendor", prefix = "vendor")}
+)
 package org.gnucash.xml.vendor;
 
 import javax.xml.bind.annotation.XmlNs;
